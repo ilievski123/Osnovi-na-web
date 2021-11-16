1dfc47678fbb3f99679418b4812b4ab7c0e85886
Filip Ilievski <ilievskifilip00@gmail.com>
2021-11-16 18:26:02 +0100

    Added javascript

---
+++ js-blog.js
@@ -0,0 +1,37 @@
+function funk(){
+    let pom1 = document.getElementById("naslov").value;
+    let pom2 = document.getElementById("blog").value;
+
+    if(pom1 == '' || pom2 == ''){
+        window.alert("Please fill in the required fields.")
+    }
+    else{
+
+    var datum = new Date();
+    var denesen = datum.getHours() + ":" + datum.getMinutes() + ":" + datum.getSeconds() + " - " + datum.getDay() + "/" + datum.getMonth() + "/" + datum.getFullYear();
+
+    const tatko = document.createElement("div");
+    let val1 = document.createElement("h2");
+    let val2 = document.createElement("div");
+    let val3 = document.createElement("div");
+
+    val3.style.float = "right";
+    val3.style.color = "grey";
+
+    val1.style.textAlign = "center";
+    val2.style.textAlign = "center";
+
+    val1.innerHTML = pom1;
+    val2.innerHTML = pom2;
+    val3.innerHTML = denesen;
+    val3.style.float = "right";
+    val3.style.color = "grey";
+    
+    tatko.appendChild(val1);
+    tatko.appendChild(val2);
+    tatko.appendChild(val3);
+
+    document.getElementById("bl").appendChild(tatko);
+
+    }
+}
---
+++ js-gallery.js
@@ -0,0 +1,175 @@
+let gallery = [
+    {
+        id: 1,
+        url: 'slika2.jpg',
+        likes: 123,
+        comments:['eden','dva'],
+        liked: 0,
+    },
+    {
+        id: 2,
+        url: 'slika3.jpg',
+        likes: 12,
+        comments:[],
+        liked: 0
+    }
+];
+
+window.onload = function(){
+    load();
+};
+function load() {
+    createGallery();
+    createFunctions();
+    createCommentFunctions();
+}
+
+function createGallery(){
+    let gal = document.getElementById("gg");
+    let gall = document.createElement("div");
+    gall.id = "tatko";
+    gallery.forEach(element => {
+        const paren = document.createElement("div");
+        paren.className += "sliki";
+        paren.id = element.id;
+        //ovoa e za slika
+        const slika = document.createElement("img");
+        slika.src = element.url;
+        slika.style.width = '500px';
+        slika.style.height = '300px';
+        paren.appendChild(slika);
+
+        //Lajkovi 
+        const cont = document.createElement("div");
+        cont.style.display = 'inline';
+        cont.style.alignItems = 'center';
+
+        const lajk = document.createElement("img");
+
+        const likes = document.createElement("div");
+        likes.innerHTML= element.likes;
+        lajk.src= "https://i.stack.imgur.com/Ui4gd.png";
+        lajk.style.width = "30px";
+        lajk.style.height = "30px";
+        lajk.style.position = "relative";
+        lajk.style.top = '20px';
+        lajk.style.left = '5px';
+
+        cont.appendChild(likes);
+        cont.appendChild(lajk);
+
+        paren.appendChild(cont);
+        paren.appendChild(document.createElement('hr'));
+        
+        // Comments
+        const kom_parent = document.createElement("div");
+        const title = document.createElement("div");
+        title.innerHTML = "Comments: ";
+        title.style.float = "left";
+        kom_parent.appendChild(title);
+
+        element.comments.forEach((el, index) => {
+            kom = document.createElement('div');
+            kom.innerHTML = el;
+            kom.style.border = "1px solid"; 
+            if (index == 0)
+                kom.style.marginTop = '50px';
+            kom_parent.appendChild(kom);
+        })
+
+        paren.appendChild(kom_parent);
+  
+        let textarea = document.createElement("Textarea");
+        textarea.rows = 10;
+        textarea.cols = 50;
+        let btt = document.createElement("button");
+        btt.innerHTML = "Comment";
+
+
+        paren.appendChild(textarea);
+        paren.appendChild(btt);
+
+
+        gall.appendChild(paren);
+
+    });
+    gal.appendChild(gall);
+
+}
+function createFunctions() {
+    gallery.forEach((element,index) => {
+        let tem = document.getElementById(element.id).childNodes[1].childNodes[1];
+        tem.addEventListener("click",lajkni.bind(null,element));
+    });
+}
+
+function createCommentFunctions() {
+    gallery.forEach((element,index) => {
+        let tem = document.getElementById(element.id).childNodes[5];
+        tem.addEventListener("click",komentiraj.bind(null,element));
+    });
+}
+
+function lajkni(param) {
+    let help = document.getElementById(param.id).childNodes[1];
+
+    if (gallery[param.id - 1].liked == 1) {
+        help.childNodes[1].src = 'https://i.stack.imgur.com/Ui4gd.png';
+        help.childNodes[0].innerHTML = parseInt(help.childNodes[0].innerHTML) - 1;
+        gallery[param.id - 1].liked = 0;
+    }
+    else {
+        help.childNodes[1].src = 'https://pngimg.com/uploads/heart/heart_PNG51341.png';
+        help.childNodes[0].innerHTML = parseInt(help.childNodes[0].innerHTML) + 1;
+        gallery[param.id - 1].liked = 1;
+    }
+
+}
+
+function komentiraj(param) {
+    let kom = document.getElementById(param.id).childNodes[4].value;
+    gallery[param.id - 1].comments.push(kom);
+
+    let  komm = document.createElement('div');
+    komm.innerHTML = kom;
+    komm.style.border = "1px solid"; 
+
+    if (document.getElementById(param.id).childNodes[3].childElementCount  <= 1)
+        komm.style.marginTop = "50px";
+    document.getElementById(param.id).childNodes[3].appendChild(komm);    
+}
+
+function addNewPhoto() {
+    document.getElementById("addKopce").style.display = "none";
+    let input = document.createElement("input");
+    input.style.position = "relative";
+    input.style.right = "10px";
+    input.id = 'novPhoto';
+    let kopce = document.createElement("button");
+    kopce.id = "kopceSlika";
+    kopce.innerHTML = "Add Photo";
+    kopce.addEventListener("click", addPhoto);
+    let parent = document.getElementById('novaSlika');
+
+    parent.appendChild(input);
+    parent.appendChild(kopce);
+}
+
+function addPhoto() {
+    let url = document.getElementById('novPhoto').value;
+    gallery.push({
+        id: gallery.length + 1,
+        url: url,
+        likes: 0,
+        comments: [],
+        liked: 0
+    });
+
+    document.getElementById("addKopce").style.display = "inline";
+    document.getElementById('kopceSlika').style.display = 'none';
+    document.getElementById('novPhoto').style.display = 'none';
+
+    document.getElementById('tatko').innerHTML = '';
+    load();
+}
+
